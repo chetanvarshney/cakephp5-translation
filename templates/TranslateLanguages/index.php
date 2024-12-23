83b4a4100b7b22643e00a6d955211e8438f5c453
Chetan Varshney <chetanvarshney@gmail.com>
2024-12-23 23:25:11 +0530

    plugin version 1.0.0

---
+++ templates/TranslateLanguages/index.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "Source code is available on https://www.codinghotspot.com/item/cakephp-5.x-translation-plugin/16";
+
+echo "Demo on https://cakephp5-translation.ektanjali.com/";
+
+?>
